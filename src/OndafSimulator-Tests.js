4d80016d2175d090b373478d2cee148ce51a72d1
Facundo Javier Gelatti <javiergelatti@gmail.com>
2016-10-20 02:05:42 -0300

    Extract widget previews to test class. Test header widget

--- src/OndafSimulator-Tests.js
+++ src/OndafSimulator-Tests.js
@@ -1,9 +1,315 @@
-define("amber-ondafsimulator/OndafSimulator-Tests", ["amber/boot", "amber_core/SUnit"], function($boot){"use strict";
+define("amber-ondafsimulator/OndafSimulator-Tests", ["amber/boot", "amber_core/SUnit", "amber_core/Kernel-Objects"], function($boot){"use strict";
 var $core=$boot.api,nil=$boot.nil,$recv=$boot.asReceiver,$globals=$boot.globals;
 $core.addPackage('OndafSimulator-Tests');
 $core.packages["OndafSimulator-Tests"].innerEval = function (expr) { return eval(expr); };
 $core.packages["OndafSimulator-Tests"].transport = {"type":"amd","amdNamespace":"amber-ondafsimulator"};
 
 $core.addClass('OndafSimulatorTest', $globals.TestCase, [], 'OndafSimulator-Tests');
+
+
+$core.addClass('WidgetPreviews', $globals.Object, [], 'OndafSimulator-Tests');
+$core.addMethod(
+$core.method({
+selector: "examTextView",
+protocol: 'widgets',
+fn: function (){
+var self=this;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx1) {
+//>>excludeEnd("ctx");
+var $1;
+$recv("body"._asJQuery())._empty();
+$1=$recv($globals.ExamTextView)._newIn_title_text_of_("body","a Title",(2),(3));
+$recv($1)._addText_("Hello ");
+$recv($1)._addWordToComplete_("Wor");
+$recv($1)._showSeconds_((99));
+$recv($1)._render();
+return self;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx1) {$ctx1.fill(self,"examTextView",{},$globals.WidgetPreviews)});
+//>>excludeEnd("ctx");
+},
+//>>excludeStart("ide", pragmas.excludeIdeData);
+args: [],
+source: "examTextView\x0a\x09'body' asJQuery empty.\x0a\x09\x0a\x09(ExamTextView newIn: 'body' title: 'a Title' text: 2 of: 3)\x0a\x09\x09addText: 'Hello ';\x0a\x09\x09addWordToComplete: 'Wor';\x0a\x09\x09showSeconds: 99;\x0a\x09\x09render.",
+referencedClasses: ["ExamTextView"],
+//>>excludeEnd("ide");
+messageSends: ["empty", "asJQuery", "addText:", "newIn:title:text:of:", "addWordToComplete:", "showSeconds:", "render"]
+}),
+$globals.WidgetPreviews);
+
+$core.addMethod(
+$core.method({
+selector: "header",
+protocol: 'widgets',
+fn: function (){
+var self=this;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx1) {
+//>>excludeEnd("ctx");
+var $1,$2;
+$1="body"._asJQuery();
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+$ctx1.sendIdx["asJQuery"]=1;
+//>>excludeEnd("ctx");
+$recv($1)._empty();
+$2=$recv($globals.Header)._new();
+$recv($2)._whenStartExam_((function(){
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx2) {
+//>>excludeEnd("ctx");
+return $recv(window)._alert_("start exam");
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx2) {$ctx2.fillBlock({},$ctx1,1)});
+//>>excludeEnd("ctx");
+}));
+$recv($2)._appendToJQuery_("body"._asJQuery());
+return self;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx1) {$ctx1.fill(self,"header",{},$globals.WidgetPreviews)});
+//>>excludeEnd("ctx");
+},
+//>>excludeStart("ide", pragmas.excludeIdeData);
+args: [],
+source: "header\x0a\x09'body' asJQuery empty.\x0a\x09\x0a\x09Header new\x0a\x09\x09whenStartExam: [ window alert: 'start exam' ];\x0a\x09\x09appendToJQuery: 'body' asJQuery.",
+referencedClasses: ["Header"],
+//>>excludeEnd("ide");
+messageSends: ["empty", "asJQuery", "whenStartExam:", "new", "alert:", "appendToJQuery:"]
+}),
+$globals.WidgetPreviews);
+
+$core.addMethod(
+$core.method({
+selector: "textCard",
+protocol: 'widgets',
+fn: function (){
+var self=this;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx1) {
+//>>excludeEnd("ctx");
+$recv("body"._asJQuery())._empty();
+$recv($globals.TextCard)._newIn_title_("body","a Title");
+return self;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx1) {$ctx1.fill(self,"textCard",{},$globals.WidgetPreviews)});
+//>>excludeEnd("ctx");
+},
+//>>excludeStart("ide", pragmas.excludeIdeData);
+args: [],
+source: "textCard\x0a\x09'body' asJQuery empty.\x0a\x09\x09\x0a\x09TextCard newIn: 'body' title: 'a Title'",
+referencedClasses: ["TextCard"],
+//>>excludeEnd("ide");
+messageSends: ["empty", "asJQuery", "newIn:title:"]
+}),
+$globals.WidgetPreviews);
+
+$core.addMethod(
+$core.method({
+selector: "textFileDropTarget",
+protocol: 'widgets',
+fn: function (){
+var self=this;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx1) {
+//>>excludeEnd("ctx");
+var $1,$2;
+$1="body"._asJQuery();
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+$ctx1.sendIdx["asJQuery"]=1;
+//>>excludeEnd("ctx");
+$recv($1)._empty();
+$recv($globals.TextFileDropTarget)._newIn_handler_("body",(function(txt){
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx2) {
+//>>excludeEnd("ctx");
+return $recv(window)._alert_(txt);
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx2) {$ctx2.fillBlock({txt:txt},$ctx1,1)});
+//>>excludeEnd("ctx");
+}));
+$2=".text-file-target"._asJQuery();
+$recv($2)._css_put_("height","100px");
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+$ctx1.sendIdx["css:put:"]=1;
+//>>excludeEnd("ctx");
+$recv($2)._css_put_("border","3px dashed gray");
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+$ctx1.sendIdx["css:put:"]=2;
+//>>excludeEnd("ctx");
+$recv($2)._css_put_("background","white");
+return self;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx1) {$ctx1.fill(self,"textFileDropTarget",{},$globals.WidgetPreviews)});
+//>>excludeEnd("ctx");
+},
+//>>excludeStart("ide", pragmas.excludeIdeData);
+args: [],
+source: "textFileDropTarget\x0a\x09'body' asJQuery empty.\x0a\x09\x09\x0a\x09TextFileDropTarget\x0a\x09\x09newIn: 'body'\x0a\x09\x09handler: [ :txt | window alert: txt ].\x0a\x09'.text-file-target' asJQuery\x0a\x09\x09css: 'height' put: '100px';\x0a\x09\x09css: 'border' put: '3px dashed gray';\x0a\x09\x09css: 'background' put: 'white'.",
+referencedClasses: ["TextFileDropTarget"],
+//>>excludeEnd("ide");
+messageSends: ["empty", "asJQuery", "newIn:handler:", "alert:", "css:put:"]
+}),
+$globals.WidgetPreviews);
+
+
+
+$core.addClass('WidgetTest', $globals.TestCase, [], 'OndafSimulator-Tests');
+$core.addMethod(
+$core.method({
+selector: "clickOn:",
+protocol: 'running',
+fn: function (aString){
+var self=this;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx1) {
+//>>excludeEnd("ctx");
+$recv(self._elementWithText_("Start exam"))._click();
+return self;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx1) {$ctx1.fill(self,"clickOn:",{aString:aString},$globals.WidgetTest)});
+//>>excludeEnd("ctx");
+},
+//>>excludeStart("ide", pragmas.excludeIdeData);
+args: ["aString"],
+source: "clickOn: aString\x0a\x09(self elementWithText: 'Start exam') click.",
+referencedClasses: [],
+//>>excludeEnd("ide");
+messageSends: ["click", "elementWithText:"]
+}),
+$globals.WidgetTest);
+
+$core.addMethod(
+$core.method({
+selector: "elementWithText:",
+protocol: 'running',
+fn: function (aString){
+var self=this;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx1) {
+//>>excludeEnd("ctx");
+var $2,$1;
+$2=$recv(":contains(".__comma(aString)).__comma(")");
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+$ctx1.sendIdx[","]=1;
+//>>excludeEnd("ctx");
+$1=$recv($2)._asJQuery();
+return $recv($1)._filter_((function(i,elem){
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx2) {
+//>>excludeEnd("ctx");
+return $recv($recv($recv(elem)._children())._length()).__eq_eq((0));
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx2) {$ctx2.fillBlock({i:i,elem:elem},$ctx1,1)});
+//>>excludeEnd("ctx");
+}));
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx1) {$ctx1.fill(self,"elementWithText:",{aString:aString},$globals.WidgetTest)});
+//>>excludeEnd("ctx");
+},
+//>>excludeStart("ide", pragmas.excludeIdeData);
+args: ["aString"],
+source: "elementWithText: aString\x0a\x09^ (':contains(', aString, ')') asJQuery\x0a\x09\x09filter: [ :i :elem | elem children length == 0 ]",
+referencedClasses: [],
+//>>excludeEnd("ide");
+messageSends: ["filter:", "asJQuery", ",", "==", "length", "children"]
+}),
+$globals.WidgetTest);
+
+$core.addMethod(
+$core.method({
+selector: "setUp",
+protocol: 'running',
+fn: function (){
+var self=this;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx1) {
+//>>excludeEnd("ctx");
+$recv("body"._asJQuery())._empty();
+return self;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx1) {$ctx1.fill(self,"setUp",{},$globals.WidgetTest)});
+//>>excludeEnd("ctx");
+},
+//>>excludeStart("ide", pragmas.excludeIdeData);
+args: [],
+source: "setUp\x0a\x09'body' asJQuery empty",
+referencedClasses: [],
+//>>excludeEnd("ide");
+messageSends: ["empty", "asJQuery"]
+}),
+$globals.WidgetTest);
+
+
+
+$core.addClass('HeaderTest', $globals.WidgetTest, [], 'OndafSimulator-Tests');
+$core.addMethod(
+$core.method({
+selector: "test01",
+protocol: 'tests',
+fn: function (){
+var self=this;
+var examStarted;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx1) {
+//>>excludeEnd("ctx");
+var $1;
+examStarted=false;
+$1=$recv($globals.Header)._new();
+$recv($1)._whenStartExam_((function(){
+examStarted=true;
+return examStarted;
+
+}));
+$recv($1)._appendToJQuery_("body"._asJQuery());
+self._assert_($recv(examStarted)._not());
+return self;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx1) {$ctx1.fill(self,"test01",{examStarted:examStarted},$globals.HeaderTest)});
+//>>excludeEnd("ctx");
+},
+//>>excludeStart("ide", pragmas.excludeIdeData);
+args: [],
+source: "test01\x0a\x09| examStarted |\x0a\x09examStarted := false.\x0a\x09\x0a\x09Header new\x0a\x09\x09whenStartExam: [ examStarted := true ];\x0a\x09\x09appendToJQuery: 'body' asJQuery.\x0a\x09\x0a\x09self assert: examStarted not",
+referencedClasses: ["Header"],
+//>>excludeEnd("ide");
+messageSends: ["whenStartExam:", "new", "appendToJQuery:", "asJQuery", "assert:", "not"]
+}),
+$globals.HeaderTest);
+
+$core.addMethod(
+$core.method({
+selector: "test02",
+protocol: 'tests',
+fn: function (){
+var self=this;
+var examStarted;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx1) {
+//>>excludeEnd("ctx");
+var $1;
+examStarted=false;
+$1=$recv($globals.Header)._new();
+$recv($1)._whenStartExam_((function(){
+examStarted=true;
+return examStarted;
+
+}));
+$recv($1)._appendToJQuery_("body"._asJQuery());
+self._clickOn_("Start exam");
+self._assert_(examStarted);
+return self;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx1) {$ctx1.fill(self,"test02",{examStarted:examStarted},$globals.HeaderTest)});
+//>>excludeEnd("ctx");
+},
+//>>excludeStart("ide", pragmas.excludeIdeData);
+args: [],
+source: "test02\x0a\x09| examStarted |\x0a\x09examStarted := false.\x0a\x09\x0a\x09Header new\x0a\x09\x09whenStartExam: [ examStarted := true ];\x0a\x09\x09appendToJQuery: 'body' asJQuery.\x0a\x09\x0a\x09self clickOn: 'Start exam'.\x0a\x09\x0a\x09self assert: examStarted",
+referencedClasses: ["Header"],
+//>>excludeEnd("ide");
+messageSends: ["whenStartExam:", "new", "appendToJQuery:", "asJQuery", "clickOn:", "assert:"]
+}),
+$globals.HeaderTest);
+
 
 });
